9271659265627ab8ba037057e58f67070df09362
Adam Mueller <adam@freerunningtech.com>
2014-08-09 10:31:45 -0700

    Remove some old unused dotfiles.

--- home/.guard.rb
+++
@@ -1,6 +0,0 @@
-# Use TMUX notifications!
-notification :tmux,
-  display_message: true,
-  default_message_format: '%s >> %s',
-  line_separator: ' > ', # since we are single line we need a separator
-  color_location: 'status-left-bg' # to customize which tmux element will change color
