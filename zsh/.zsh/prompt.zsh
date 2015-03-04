375b59b020e8422a2649944c7e80535c41f467fe
Adam Mueller <adam@freerunningtech.com>
2015-03-04 08:46:52 -0800

    Use vcs_info for prompt.

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,23 +1,15 @@
-function parse_git_dirty {
-  # Are we in a repo?
-  [[ "$(command git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] || return
-  # Check to see if it's dirty!
-  command test -n "$(git status --porcelain --ignore-submodules ${umode})"
-  (($? == 0)) && echo "%F{red}%f"
-}
-
-function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1$(parse_git_dirty)/"
-}
-
-function git_info {
-  [[ "$(parse_git_branch)" != "" ]] || return
-  echo "%F{blue}git%f:%F{cyan}$(parse_git_branch)%f "
-}
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:git*' formats "%F{blue}%s%f:%F{cyan}%b%f(%u%c) "
+zstyle ':vcs_info:*' actionformats "%F{blue}%s%f:%F{cyan}%b%f %F{red}(%a)%f(%u%c) "
+zstyle ':vcs_info:*' stagedstr "%F{green}%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}%f"
+precmd() { vcs_info }
 
 local current_dir="%F{gray}%~%f "
 local user_prompt="%B%F{green}%#%f%b "
-PROMPT='${current_dir}$(git_info)${user_prompt}'
+PROMPT='${current_dir}${vcs_info_msg_0_}${user_prompt}'
 
 function zle-line-init zle-keymap-select {
   local vim_prompt="%K{cyan}%F{white}%B NORMAL %b%f%k"
