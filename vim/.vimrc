2d85343b6875b720d96291850e92f70ddaa290cd
Adam Mueller <adam@freerunningtech.com>
2015-12-18 13:11:18 -0800

    Use unicode escapes for special characters

--- vim/.vimrc
+++ vim/.vimrc
@@ -98,10 +98,10 @@ let g:airline_left_alt_sep = ''
 let g:airline_right_sep = ''
 let g:airline_right_alt_sep = ''
 
-let g:airline_symbols.branch = ''
-let g:airline_symbols.readonly = ''
-let g:airline_symbols.linenr = ''
-let g:airline_symbols.whitespace = 'Ξ'
+let g:airline_symbols.branch = "\uf126"
+let g:airline_symbols.readonly = "\uf023 "
+let g:airline_symbols.linenr = ""
+let g:airline_symbols.whitespace = "Ξ"
 
 let g:airline_theme = 'solarized'
 
--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -3,8 +3,8 @@ zstyle ':vcs_info:*' enable git
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:git*' formats "%F{blue}%s%f:%F{cyan}%b%f(%u%c) "
 zstyle ':vcs_info:*' actionformats "%F{blue}%s%f:%F{cyan}%b%f %F{red}(%a)%f(%u%c) "
-zstyle ':vcs_info:*' stagedstr "%F{green}%f"
-zstyle ':vcs_info:*' unstagedstr "%F{red}%f"
+zstyle ':vcs_info:*' stagedstr "%F{green}+%f"
+zstyle ':vcs_info:*' unstagedstr "%F{red}-%f"
 precmd() { vcs_info }
 
 local suspended_jobs="%(1j.[%F{blue}%j%f] .)"
